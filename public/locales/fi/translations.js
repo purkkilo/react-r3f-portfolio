1953ef4f6a94e558e925d1c5bd909452c299f133
Joriko <jori.kosonen@student.lut.fi>
2025-03-23 18:03:58 +0200

    Improved translation readability

--- public/locales/fi/translations.js
+++ public/locales/fi/translations.js
@@ -1,13 +1,20 @@
 const description =
-    'Olen rauhallinen korkean työmoraalin omaava verkko- ja ohjelmistokehityksestä kiinnostunut yksilö. Kokemukseni LUT-yliopistossa ja henkilökohtaisissa projekteissa ovat antaneet minulle monenlaisia taitoja varsinkin frontend-kehitykseen, ja olen aina kiinnostunut oppimaan lisää. Omaan kanditaatin tutkinnon LUT-yliopistosta ja tällä hetkellä maisterintutkintoni (Software Engineering and Digital Transformation) on opinnäytetyötä vaille valmis, ja aktiivisesti etsin aihetta työtä varten jotta saisin opintoni päätökseen. Tykkään ratkoa erilaisia pulmia, vapaa-ajalla harrastan saleilua, elokuvien katsomista ja kaikenlaisia pulmapelejä ARG jutuista koodauspulmiin ja shakkiin.'
+    `Olen rauhallinen korkean työmoraalin omaava verkko- ja ohjelmistokehityksestä kiinnostunut yksilö.
+    Kokemukseni LUT-yliopistossa ja henkilökohtaisissa projekteissa ovat antaneet minulle monenlaisia taitoja
+    varsinkin frontend-kehitykseen, ja olen aina kiinnostunut oppimaan lisää.
+    Omaan kanditaatin tutkinnon LUT-yliopistosta ja tällä hetkellä maisterintutkintoni
+    (Software Engineering and Digital Transformation) on opinnäytetyötä vaille valmis,
+    ja aktiivisesti etsin aihetta työtä varten jotta saisin opintoni päätökseen.
+    Tykkään ratkoa erilaisia pulmia, vapaa-ajalla harrastan saleilua,
+    elokuvien katsomista ja kaikenlaisia pulmapelejä ARG jutuista koodauspulmiin ja shakkiin.`
 
-  const fisustajaDescription = `Verkkosovellus kalastuskilpailujen pitämiseen, joko Cup kokonaisuutena tai yksittäin.
+const fisustajaDescription = `Verkkosovellus kalastuskilpailujen pitämiseen, joko Cup kokonaisuutena tai yksittäin.
   MongoDB, JavaScript and Vue 2 stack. Projektin tarkoitus oli opetella edellämainittuja teknologioita. *Projekti pyörii ilmaiseksi Renderissä, joten voi kestää minuutin kunnes serveri käynnistyy.
   `
-  const webgisDescription = `Verkkosovellus silkkitien ja sen varrella olevien kaupunkien tarkastelemiseen.
+const webgisDescription = `Verkkosovellus silkkitien ja sen varrella olevien kaupunkien tarkastelemiseen.
   Tehty yliopiston GIS (Geographic Information Systems) kurssia varten. Silkkitien lisäksi matkalta näkyy kaupungit ja kolme lähintä hotellia.
   `
-  const portfolioDescription = `Verkkosivusto tehty React, Next.js ja TailwindCSS teknologioilla.
+const portfolioDescription = `Verkkosivusto tehty React, Next.js ja TailwindCSS teknologioilla.
   Taustan "tähdet" on tehty Three.js ja React-three-fiber:llä.
   Sivusto on tehty esittelemään taitojani ja projektejani. Sivu on saatavilla englanniksi sekä suomeksi.`
 
